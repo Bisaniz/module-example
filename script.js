95d64964169194a2da4725fb34c2d5e3b055726e
Bisaniz <bisandawabsheh@gmail.com>
2023-10-23 19:19:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// import {user} from './user.js';
+// user.displayinfo();
+
+
+import u, { display as displayTest, display2} from './user.js'
+console.log(u);
+displayTest()
+display2()
---
+++ user.js
@@ -0,0 +1,21 @@
+// const user = {
+//     name: 'John Doe',
+//     email: 'john@example.com'
+//     };
+//     // Export the user object as a module
+//     export default user;
+//     //export {displayuserinfo};
+
+
+export default {
+    name:"hala",
+    age:23
+    }
+    export function display (){
+    console.log("test");
+    }
+    export function display2 (){
+    console.log("test2");
+    }
+    // export default user
+    // export {display,display2}
